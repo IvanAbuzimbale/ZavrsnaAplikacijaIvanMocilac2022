d5eea7b21c7fa2bdf23fdb07731a90b080f18771
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2022-03-14 09:26:55 +0100

    Add files via upload

---
+++ ZavrsnaAplikacijaIvanMocilac2022/ZavrsnaAplikacijaIvanMocilac2022/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace ZavrsnaAplikacijaIvanMocilac2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnNoviObjekt = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(50, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Upis novog objekta";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(334, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Upis nove rezervacije";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(50, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Upis novog gosta";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(334, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Pregled i ispis rezervacije";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(211, 414);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 32);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Zatvori";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNoviObjekt
+            // 
+            this.btnNoviObjekt.BackColor = System.Drawing.Color.Transparent;
+            this.btnNoviObjekt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnNoviObjekt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNoviObjekt.Image = ((System.Drawing.Image)(resources.GetObject("btnNoviObjekt.Image")));
+            this.btnNoviObjekt.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnNoviObjekt.Location = new System.Drawing.Point(50, 36);
+            this.btnNoviObjekt.Name = "btnNoviObjekt";
+            this.btnNoviObjekt.Size = new System.Drawing.Size(105, 108);
+            this.btnNoviObjekt.TabIndex = 10;
+            this.btnNoviObjekt.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(337, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 108);
+            this.button2.TabIndex = 11;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(337, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 93);
+            this.button3.TabIndex = 12;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(50, 236);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 93);
+            this.button4.TabIndex = 13;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(203, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 79);
+            this.button1.TabIndex = 14;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(480, 472);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnNoviObjekt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Močilius - Odabir usluge";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnNoviObjekt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ZavrsnaAplikacijaIvanMocilac2022/ZavrsnaAplikacijaIvanMocilac2022/FormUpisNovogObjekta.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace ZavrsnaAplikacijaIvanMocilac2022
+{
+    partial class FormUpisNovogObjekta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormUpisNovogObjekta));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtImeObjekta = new System.Windows.Forms.TextBox();
+            this.txtVlasnikObjekta = new System.Windows.Forms.TextBox();
+            this.txtCijena = new System.Windows.Forms.TextBox();
+            this.btnUpisi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime objekta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vlasnik objekta:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cijena po danu:";
+            // 
+            // txtImeObjekta
+            // 
+            this.txtImeObjekta.Location = new System.Drawing.Point(99, 9);
+            this.txtImeObjekta.Name = "txtImeObjekta";
+            this.txtImeObjekta.Size = new System.Drawing.Size(100, 20);
+            this.txtImeObjekta.TabIndex = 3;
+            // 
+            // txtVlasnikObjekta
+            // 
+            this.txtVlasnikObjekta.Location = new System.Drawing.Point(99, 44);
+            this.txtVlasnikObjekta.Name = "txtVlasnikObjekta";
+            this.txtVlasnikObjekta.Size = new System.Drawing.Size(100, 20);
+            this.txtVlasnikObjekta.TabIndex = 4;
+            // 
+            // txtCijena
+            // 
+            this.txtCijena.Location = new System.Drawing.Point(99, 82);
+            this.txtCijena.Name = "txtCijena";
+            this.txtCijena.Size = new System.Drawing.Size(100, 20);
+            this.txtCijena.TabIndex = 5;
+            // 
+            // btnUpisi
+            // 
+            this.btnUpisi.BackColor = System.Drawing.Color.Transparent;
+            this.btnUpisi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnUpisi.BackgroundImage")));
+            this.btnUpisi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnUpisi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpisi.Location = new System.Drawing.Point(99, 159);
+            this.btnUpisi.Name = "btnUpisi";
+            this.btnUpisi.Size = new System.Drawing.Size(72, 85);
+            this.btnUpisi.TabIndex = 6;
+            this.btnUpisi.UseVisualStyleBackColor = false;
+            // 
+            // FormUpisNovogObjekta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 256);
+            this.Controls.Add(this.btnUpisi);
+            this.Controls.Add(this.txtCijena);
+            this.Controls.Add(this.txtVlasnikObjekta);
+            this.Controls.Add(this.txtImeObjekta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormUpisNovogObjekta";
+            this.Text = "Upis novog objekta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtImeObjekta;
+        private System.Windows.Forms.TextBox txtVlasnikObjekta;
+        private System.Windows.Forms.TextBox txtCijena;
+        private System.Windows.Forms.Button btnUpisi;
+    }
+}
